a8d615e2687f928d0f50f70d470778787e794731
Victor Buga <bugavictor86@gmail.com>
2023-02-08 23:31:17 +0100

    header modif

--- castle-app/src/components/common/header.jsx
+++ castle-app/src/components/common/header.jsx
@@ -28,29 +28,21 @@ export default function Header() {
     setAnimated(!isAnimated);
   };
   //-------------HIDE-NAVBAR-ON-SCROLL-----------------------------
-  document.addEventListener("DOMContentLoaded", function () {
-    const el_autohide = document.querySelector(".autohide");
-    // add padding-top to bady (if necessary)
-    const navbar_height = document.querySelector("#nav").offsetHeight;
-    document.body.style.paddingTop = navbar_height + "px";
-
-    if (el_autohide) {
-      let last_scroll_top = 0;
-      window.addEventListener("scroll", function () {
-        let scroll_top = window.scrollY;
-        if (scroll_top < last_scroll_top) {
-          el_autohide.classList.remove("scrolled-down");
-          el_autohide.classList.add("scrolled-up");
-        } else {
-          el_autohide.classList.remove("scrolled-up");
-          el_autohide.classList.add("scrolled-down");
-        }
-        last_scroll_top = scroll_top;
-      });
-      // window.addEventListener
-    }
-    // if
-  });
+  const [lastScroll, setLastScroll] = useState(0);
+  const el_autohide = document.querySelector(".autohide");
+  if (el_autohide) {
+    window.addEventListener("scroll", function () {
+      const scrollTop = window.scrollY;
+      if (scrollTop < lastScroll) {
+        el_autohide.classList.remove("scrolled-down");
+        el_autohide.classList.add("scrolled-up");
+      } else {
+        el_autohide.classList.remove("scrolled-up");
+        el_autohide.classList.add("scrolled-down");
+      }
+      setLastScroll(scrollTop);
+    });
+  }
 
   return (
     <div className="container_header">
