2e04a1fc34e04dc9cd4cb2b61f61afe511e0d5d4
Victor Buga <bugavictor86@gmail.com>
2023-02-11 10:12:16 +0100

    del br

--- castle-app/src/components/home/introduction.jsx
+++ castle-app/src/components/home/introduction.jsx
@@ -9,28 +9,19 @@ export default function Accueil() {
           <h3>Le plus meublé des château royaux français</h3>
           <p>{introduction.le_plus_meuble}</p>
           <p>{introduction.patrimoine}</p>
-          <br />
           <p>{introduction.emblematique}</p>
-          <br />
           <p>{introduction.appartements}</p>
-          <br />
           <p>{introduction.vie_au_chateau}</p>
-          <br />
           <p className="source">Soure: www.fontainebleau-tourisme.com/</p>
         </article>
         <aside>
           <div>
             <h3>Les Origines</h3>
             <p>{introduction.origines}</p>
-            <br />
             <p>{introduction.saint_louis}</p>
-            <br />
             <p>{introduction.guerre_cent_ans}</p>
-            <br />
             <p>{introduction.francois_premier}</p>
-            <br />
             <p>{introduction.charles_ix}</p>
-            <br />
             <p className="source">
               Source: www.chateaudeharoue.fr/le-chateau-de-fontainebleau/
             </p>
