f5f20ff38eda6660c0ebc220fbe663a4fc10e58b
Victor Buga <bugavictor86@gmail.com>
2025-01-30 16:25:18 +0100

    new cluster

--- castle-api/connect_mongodb/mongodb.js
+++ castle-api/connect_mongodb/mongodb.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 //import mongoose from 'mongoose' === ERROR
 
-mongoose.connect('mongodb+srv://boogysh:' + process.env.MONGO_DB_PASSWORD + '@cluster0.m2vegey.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect('mongodb+srv://boogysh:' + process.env.MONGO_DB_PASSWORD + 'cluster0.lanjt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 ,
+//cluster0.lanjt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0   --- new cluster ---
+//@cluster0.m2vegey.mongodb.net/?retryWrites=true&w=majority   --- old cluster---
 { useNewUrlParser: true,
   useUnifiedTopology: true 
 },)
