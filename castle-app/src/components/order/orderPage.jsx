e8fb119bd76830034bdd339fdb10a99e02feb713
Victor Buga <bugavictor86@gmail.com>
2025-02-13 12:42:01 +0100

    fix url vercel

--- castle-app/src/components/order/orderPage.jsx
+++ castle-app/src/components/order/orderPage.jsx
@@ -154,7 +154,7 @@ export default function OrderPage() {
     e.preventDefault();
     if (allValues) {
       // fetch(`${env.API_URL_ORDER}`, {
-      fetch(`https://ccastle-637wiwwcy-boogyshs-projects.vercel.app/api/commandes`, {
+      fetch(`https://castle-4zrcgold4-boogyshs-projects.vercel.app/api/commandes`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(order),
